cb13e756981f063969ce1a08f47d659d77d45480
rokoe <koen.roevens@gmail.com>
2014-12-03 06:01:40 +0100

    Added support to install/deploy of attached artifact in one go to releases and snapshots repository

--- mvn-ext-freeze/src/main/java/be/fluid_it/mvn/cd/x/freeze/execution/SnapshotExecutionListener.java
+++ mvn-ext-freeze/src/main/java/be/fluid_it/mvn/cd/x/freeze/execution/SnapshotExecutionListener.java
@@ -6,22 +6,19 @@ import org.apache.maven.artifact.ArtifactUtils;
 import org.apache.maven.execution.AbstractExecutionListener;
 import org.apache.maven.execution.ExecutionEvent;
 import org.apache.maven.project.MavenProject;
-import org.apache.maven.project.artifact.ProjectArtifactMetadata;
 import org.codehaus.plexus.logging.Logger;
 
 import java.io.File;
+import java.util.LinkedList;
+import java.util.List;
 
 public class SnapshotExecutionListener extends AbstractExecutionListener {
     private final Logger logger;
 
-    private Artifact artifact;
-    private File pomFile;
-
-    private File artifactFile;
-    private String finalName;
-
-    private String version;
-    private ProjectArtifactMetadata projectArtifactMetadata;
+    private Artifact frozenArtifact;
+    private File frozenPomFile;
+    private String frozenFinalName;
+    private List<Artifact> frozenAttachedArtifacts;
 
     public SnapshotExecutionListener(final Logger logger) {
         this.logger = logger;
@@ -34,37 +31,35 @@ public class SnapshotExecutionListener extends AbstractExecutionListener {
     @Override
     public void mojoStarted( ExecutionEvent event )
     {
-
         if (event.getMojoExecution().getExecutionId() != null && event.getMojoExecution().getExecutionId().endsWith("-snapshot")) {
 
-            pomFile = event.getProject().getFile();
-            event.getProject().setFile(new File( pomFile.getParent(), MavenConventions.POM_FILE));
+            frozenPomFile = event.getProject().getFile();
+            event.getProject().setFile(new File( frozenPomFile.getParent(), MavenConventions.POM_FILE));
 
-            artifact = event.getProject().getArtifact();
-            version = event.getProject().getArtifact().getVersion();
-            Artifact snapshotArtifact = ArtifactUtils.copyArtifact(artifact);
-            snapshotArtifact.setVersion(transformToSnapshot(artifact.getVersion()));
+            frozenArtifact = event.getProject().getArtifact();
+            Artifact snapshotArtifact = ArtifactUtils.copyArtifact(frozenArtifact);
+            snapshotArtifact.setVersion(transformToSnapshot(frozenArtifact.getVersion()));
             event.getProject().setArtifact(snapshotArtifact);
 
             if (!isPomArtifact(event.getProject())) {
-                finalName = event.getProject().getBuild().getFinalName();
-                event.getProject().getBuild().setFinalName(transformToSnapshot(finalName));
+                frozenFinalName = event.getProject().getBuild().getFinalName();
+                event.getProject().getBuild().setFinalName(transformToSnapshot(frozenFinalName));
 
-                // TODO
-                event.getProject().getArtifact().setFile(new File(transformToSnapshot(artifact.getFile().getAbsolutePath()) + ".jar"));
-                logger.info("Switched artifact file to " + transformToSnapshot(artifact.getFile().getAbsolutePath()) + ".jar");
-                /*
-                for (ArtifactMetadata artifactMetadata : event.getProject().getArtifact().getMetadataList()) {
-                    if (artifactMetadata instanceof ProjectArtifactMetadata) {
-                        logger.info(">>>> ProjectArtifactMetadata file : " + ((ProjectArtifactMetadata)artifactMetadata).getFile());
-                        projectArtifactMetadata = (ProjectArtifactMetadata)artifactMetadata;
-                    }
-                }
+                event.getProject().getArtifact().setFile(new File(transformToSnapshot(frozenArtifact.getFile().getAbsolutePath()) + ".jar"));
+                logger.info("Switched frozenArtifact file to " + transformToSnapshot(frozenArtifact.getFile().getAbsolutePath()) + ".jar");
+            }
+            frozenAttachedArtifacts = new LinkedList<Artifact>();
 
-                if (projectArtifactMetadata != null) {
-                    event.getProject().getArtifact().getMetadataList().remove(projectArtifactMetadata);
-                }
-                */
+            for (Artifact attachedArtifact : event.getProject().getAttachedArtifacts()) {
+                String attachedFileAbsolutePath = attachedArtifact.getFile().getAbsolutePath();
+                logger.info(">>>> Attached frozenArtifact file : " + attachedFileAbsolutePath);
+                frozenAttachedArtifacts.add(attachedArtifact);
+            }
+            event.getProject().getAttachedArtifacts().clear();
+            for (Artifact frozenAttachedArtifact : frozenAttachedArtifacts) {
+                Artifact unfrozenAttachedArtifact = ArtifactUtils.copyArtifact(frozenAttachedArtifact);
+                unfrozenAttachedArtifact.setVersion(transformToSnapshot(unfrozenAttachedArtifact.getVersion()));
+                event.getProject().addAttachedArtifact(unfrozenAttachedArtifact);
             }
         }
     }
@@ -72,7 +67,6 @@ public class SnapshotExecutionListener extends AbstractExecutionListener {
     private String transformToSnapshot(String data) {
         String[] parts = data.split("-");
         StringBuffer result = new StringBuffer();
-        String[] lastParts = parts[parts.length -1].split(".");
         for (int i = 0; i < parts.length -1; i++) {
             result.append(parts[i]).append("-");
         }
@@ -91,24 +85,19 @@ public class SnapshotExecutionListener extends AbstractExecutionListener {
 
     private void restoreProjectState(ExecutionEvent event) {
         if (event.getMojoExecution().getExecutionId() != null && event.getMojoExecution().getExecutionId().endsWith("-snapshot")) {
-            if (!event.getProject().getFile().equals(pomFile)) {
-                event.getProject().setFile(pomFile);
-                //event.getProject().getArtifact().setVersion(version);
-                event.getProject().setArtifact(artifact);
+            if (!event.getProject().getFile().equals(frozenPomFile)) {
+                event.getProject().setFile(frozenPomFile);
+                event.getProject().setArtifact(frozenArtifact);
+
+                event.getProject().getAttachedArtifacts().clear();
+                for (Artifact frozenAttachedArtifact : frozenAttachedArtifacts) {
+                    event.getProject().addAttachedArtifact(frozenAttachedArtifact);
+                }
 
                 if (!isPomArtifact(event.getProject())) {
-                    event.getProject().getBuild().setFinalName(finalName);
-                    /*
-                    event.getProject().getArtifact().setFile(artifactFile);
-
-                    if (projectArtifactMetadata != null) {
-                        event.getProject().getArtifact().getMetadataList().add(projectArtifactMetadata);
-                    }
-                    */
+                    event.getProject().getBuild().setFinalName(frozenFinalName);
                 }
             }
         }
     }
-
-
 }
